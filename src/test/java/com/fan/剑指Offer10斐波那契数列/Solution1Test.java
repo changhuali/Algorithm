2795c69f7a6575168c17a10a584fa3830577b07c
changhuali <changhua.li.cq@gmail.com>
2021-07-24 15:18:13 +0800

    add: 剑指Offer10斐波那契数列解法1

---
+++ src/test/java/com/fan/剑指Offer10斐波那契数列/Solution1Test.java
@@ -0,0 +1,17 @@
+package com.fan.剑指Offer10斐波那契数列;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("剑指Offer10斐波那契数列")
+class Solution1Test {
+
+    @Test
+    void fib() {
+        Solution1 solution1 = new Solution1();
+        assertEquals(1, solution1.fib(1));
+        assertEquals(687995182, solution1.fib(100));
+    }
+}
