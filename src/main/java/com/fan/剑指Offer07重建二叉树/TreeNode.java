28a1d8ee69abebcee27fcd9dbc17f33768257bf1
changhuali <changhua.li.cq@gmail.com>
2021-07-24 14:33:54 +0800

    add: 剑指Offer07重建二叉树解法1

---
+++ src/main/java/com/fan/剑指Offer07重建二叉树/TreeNode.java
@@ -0,0 +1,11 @@
+package com.fan.剑指Offer07重建二叉树;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int nodeVal) {
+        val = nodeVal;
+    }
+}
