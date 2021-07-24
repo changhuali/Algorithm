5ed977b87a2adc2759c650a7e84fa1cd560a651f
changhuali <changhua.li.cq@gmail.com>
2021-07-24 14:33:33 +0800

    add: 剑指Offer04二维数组中的查找解法1

---
+++ src/main/java/com/fan/剑指Offer04二维数组中的查找/Solution1.java
@@ -0,0 +1,21 @@
+package com.fan.剑指Offer04二维数组中的查找;
+
+class Solution1 {
+    public boolean findNumberIn2DArray(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+        int rows = matrix.length, cols = matrix[0].length;
+        int i = 0, j = cols - 1;
+        while (i < rows && j >= 0) {
+            int cur = matrix[i][j];
+            if (cur == target) return true;
+            if (target > cur) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return false;
+    }
+}
