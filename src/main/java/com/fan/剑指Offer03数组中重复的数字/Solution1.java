591509c52bae40347aeeebd3cc26b4f0270abf2b
changhuali <changhua.li.cq@gmail.com>
2021-07-24 14:32:50 +0800

    add: 剑指Offer03数组中重复的数字解法1

---
+++ src/main/java/com/fan/剑指Offer03数组中重复的数字/Solution1.java
@@ -0,0 +1,17 @@
+package com.fan.剑指Offer03数组中重复的数字;
+
+import java.util.HashMap;
+
+public class Solution1 {
+    public int findRepeatNumber(int[] numbers) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < numbers.length; i++) {
+            int cur = numbers[i];
+            if (map.containsKey(cur)) {
+                return cur;
+            }
+            map.put(cur, i);
+        }
+        return  - 1;
+    }
+}
