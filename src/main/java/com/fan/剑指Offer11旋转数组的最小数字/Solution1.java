5d444576d63e5c52d14eef4b3c4e35f47a440a04
changhuali <changhua.li.cq@gmail.com>
2021-07-25 22:19:32 +0800

    add: 剑指Offer11旋转数组的最小数字解法1

---
+++ src/main/java/com/fan/剑指Offer11旋转数组的最小数字/Solution1.java
@@ -0,0 +1,16 @@
+package com.fan.剑指Offer11旋转数组的最小数字;
+
+public class Solution1 {
+    public int minArray(int[] numbers) {
+        int len = numbers.length;
+        if (len == 0) {
+            return -1;
+        }
+        for (int i = 0; i < len - 1; i++) {
+            if (numbers[i+1] < numbers[i]) {
+                return numbers[i+1];
+            }
+        }
+        return numbers[0];
+    }
+}
