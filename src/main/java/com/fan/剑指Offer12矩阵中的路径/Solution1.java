99c6c70560ccec4662cc6f7ffe085519257236e9
changhuali <changhua.li.cq@gmail.com>
2021-07-26 13:21:36 +0800

    refactor: 剑指Offer12矩阵中的路径解法1

--- src/main/java/com/fan/剑指Offer12矩阵中的路径/Solution1.java
+++ src/main/java/com/fan/剑指Offer12矩阵中的路径/Solution1.java
@@ -2,9 +2,10 @@ package com.fan.剑指Offer12矩阵中的路径;
 
 public class Solution1 {
     public boolean exist(char[][] board, String word) {
+        char[] words = word.toCharArray();
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                if (dfs(board, word.toCharArray(), i, j, 0)) return true;
+                if (dfs(board, words, i, j, 0)) return true;
             }
         }
         return false;
