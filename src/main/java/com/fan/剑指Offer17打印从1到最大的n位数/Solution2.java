bf2e75a7dda3d11b630265be04a947541706e090
changhuali <changhua.li.cq@gmail.com>
2021-08-01 21:53:06 +0800

    add: 剑指Offer17打印从1到最大的n位数解法1、解法2

---
+++ src/main/java/com/fan/剑指Offer17打印从1到最大的n位数/Solution2.java
@@ -0,0 +1,21 @@
+package com.fan.剑指Offer17打印从1到最大的n位数;
+
+public class Solution2 {
+    static final char[] chars = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+    public String printNumbers(int n) {
+        return dfs("", n);
+    }
+    private String dfs(String prefix, int n) {
+        if (n == 0) return prefix + ',';
+        String str = "";
+        for (char c: chars) {
+            str += dfs(prefix + c, n - 1);
+        }
+        return str;
+    }
+
+    public static void main(String[] args) {
+        Solution2 solution2 = new Solution2();
+        System.out.println(solution2.printNumbers(3));
+    }
+}
---
+++ src/test/java/com/fan/剑指Offer17打印从1到最大的n位数/Solution1Test.java
@@ -0,0 +1,24 @@
+package com.fan.剑指Offer17打印从1到最大的n位数;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("剑指Offer17打印从1到最大的n位数解法1")
+class Solution1Test {
+
+    @Test
+    void printNumbers() {
+        Solution1 solution1 = new Solution1();
+        assertArrayEquals(new int[]{1,2,3,4,5,6,7,8,9}, solution1.printNumbers(1));
+
+        int[] arr2 = new int[99];
+        int i2 = 1;
+        while (i2 < 100) {
+            arr2[i2 - 1] = i2;
+            i2++;
+        }
+        assertArrayEquals(arr2, solution1.printNumbers(2));
+    }
+}
