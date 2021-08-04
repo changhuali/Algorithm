d0c03d0048c660e7c0b8eb235fc1f72e4e5036db
changhuali <changhua.li.cq@gmail.com>
2021-08-04 18:14:27 +0800

    refactor: 剑指Offer19正则表达式匹配解法1

--- src/main/java/com/fan/剑指Offer19正则表达式匹配/Solution1.java
+++ src/main/java/com/fan/剑指Offer19正则表达式匹配/Solution1.java
@@ -3,32 +3,36 @@ package com.fan.剑指Offer19正则表达式匹配;
 class Solution1 {
     public boolean isMatch(String s, String p) {
         int m = s.length(), n = p.length();
-        if (m == 0 && n == 0 || (n == 2 && p.charAt(1) == '*' && m == 0)) return true;
-        if (m * n == 0) return false;
-        char firstS = s.charAt(0), firstP = p.charAt(0);
-        char nextP = n > 1 ? p.charAt(1) : '\0';
-        if (nextP == '*') {
+        if (m == 0 && n == 0) return true;
+        if (m > 0 && n == 0) return false;
+        char fcInS = m > 0 ? s.charAt(0) : '\0', fcInP = p.charAt(0);
+        char scInP = n > 1 ? p.charAt(1) : '\0';
+        if (scInP == '*') {
             String restP = p.substring(2);
-            int i = 0, j = 0;
-            while (i < m && (s.charAt(i) == firstP || firstP == '.')) {
-                i++;
+            int k = 0;
+            while (k < m && isCharMatch(s.charAt(k), fcInP)) {
+                k++;
             }
-            while (i >= 0) {
-                if (isMatch(s.substring(i), restP)) {
+            while (k >= 0) {
+                if (isMatch(s.substring(k), restP)) {
                     return true;
                 }
-                i--;
+                k--;
             }
         } else {
-            if (firstS == firstP || firstP == '.') {
+            if (isCharMatch(fcInS, fcInP)) {
                 return isMatch(s.substring(1), p.substring(1));
             }
         }
         return false;
     }
 
+    private boolean isCharMatch(char a, char b) {
+        return a != '\0' && (a == b || b == '.');
+    }
+
     public static void main(String[] args) {
         Solution1 solution1 = new Solution1();
-        solution1.isMatch("", ".*");
+        System.out.println(solution1.isMatch("", "."));
     }
 }
